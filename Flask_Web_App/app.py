6d84b3499e190f69240dcdba6e19091fb270525a
Durdana_Khalid <durdanakhalid98@gmail.com>
2025-07-26 21:04:17 +0500

    Update app.py

--- Flask_Web_App/app.py
+++ Flask_Web_App/app.py
@@ -37,5 +37,6 @@ def index():
 
     return render_template('index.html', recommendations=recommendations, movie_name=movie_name)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # use Railway port or default to 8080
+    app.run(host="0.0.0.0", port=port) 
